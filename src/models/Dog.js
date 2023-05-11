50e68769014b9bccf70f5bb7d6bda050b9366cf5
unknown <ddandolo@yahoo.com>
2023-05-11 10:50:11 -0300

    haciendo modelo Dog

--- src/models/Dog.js
+++ src/models/Dog.js
@@ -8,8 +8,29 @@ module.exports = (sequelize) => {
   // defino el modelo
   
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    weight: {
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
+
+    height: {
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
+
+    bred_for: {
+      type: DataTypes.TEXT,
       allowNull: false,
     },
   });
