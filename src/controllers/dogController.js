09ac561d92f1b744bd4db0c60b448aa1dd19f506
unknown <ddandolo@yahoo.com>
2023-05-31 13:15:36 -0300

    correccion

--- src/controllers/dogController.js
+++ src/controllers/dogController.js
@@ -55,7 +55,6 @@ const getAllDogs = async (req, res) => {
         let apiDogs = await axios.get(API_URL);
         let dbDogs = await Dog.findAll({include: {model: Temper}});
         let allDogs = [...apiDogs.data, ...dbDogs];
-
         if(allDogs.length === 0) {
             return res.status(404).json({message: "No dogs where found"});
         }
@@ -108,7 +107,7 @@ const getDogById = async (req, res) => {
             return res.status(200).json(dbDogs);
         } else {
         let apiDogs = await axios.get(API_URL);
-        let apiFiltered = await apiDogs.data.filter(dog => dog.id.toString() === id.toString());
+        let apiFiltered = await apiDogs.data.find(dog => dog.id.toString() === id.toString());
 
             if(apiFiltered.length === 0) {
                 return res.status(404).json({message: `Dog with id ${id} not found`});
