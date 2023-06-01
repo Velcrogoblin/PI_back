f075183bf82b218957031cdc9e7929911972d209
unknown <ddandolo@yahoo.com>
2023-06-01 01:53:48 -0300

    resetee tempers

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {uploadTempers} = require("./src/controllers/temperController.js");
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-uploadTempers();
+//uploadTempers();
 server.listen(PORT, () => {
     console.log(`listening at ${PORT}`); // eslint-disable-line no-console
   });
