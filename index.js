28267a763349dfb9b19f92650107988700077f4b
unknown <ddandolo@yahoo.com>
2023-06-07 15:44:06 -0300

    error

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const { PORT } = process.env;
 const {uploadTempers} = require("./src/controllers/temperController.js");
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-//uploadTempers();
+conn.sync({ force: true }).then(() => {
+uploadTempers();
 server.listen(PORT, () => {
     console.log(`listening at ${PORT}`); // eslint-disable-line no-console
   });
