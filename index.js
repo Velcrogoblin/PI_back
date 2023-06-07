1e29d1b107b0b3090454489e2983338a4732cb7b
unknown <ddandolo@yahoo.com>
2023-06-07 15:57:36 -0300

    jdkslad

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const {uploadTempers} = require("./src/controllers/temperController.js");
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
 uploadTempers();
+//pruebas
 server.listen(PORT, () => {
     console.log(`listening at ${PORT}`); // eslint-disable-line no-console
   });
